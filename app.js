516e6577ec27b0f010ee6725c04f4a52acac3018
CodeHelp by Rahul <113056235+CodeHelp1@users.noreply.github.com>
2023-01-22 11:46:54 +0530

    app.js backend

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true,
+    credentials: false,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
